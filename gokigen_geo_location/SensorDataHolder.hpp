5b6f2d1d87e414d144c8e4e08361f9aefb30eb5a
MRSa <mrsa@myad.jp>
2025-09-04 23:10:43 +0900

    画面右上にマーカーを出して、何か操作ができることを示してみた。

--- gokigen_geo_location/SensorDataHolder.hpp
+++ gokigen_geo_location/SensorDataHolder.hpp
@@ -6,6 +6,8 @@ private:
   Adafruit_BMP280 bmp;
   double _temperature = 0.0d;  // 温度
   double _pressure = 0.0d;     // 圧力
+  double _altitude = 0.0d;     // 高度
+  double _waterBoilingPoint = 100.0d; // 沸点
   uint8_t _batteryLevel = 0;   // バッテリ残量
   struct tm _timeInfo;         // 時刻情報
 
@@ -18,18 +20,20 @@ public:
   uint8_t getBatteryLevel() { return _batteryLevel; }
   double getTemperature() { return _temperature; }
   double getPressure() { return _pressure; }
+  double getAltitude() { return _altitude; }
+  double getWaterBoilingPoint() { return _waterBoilingPoint; } 
   struct tm *getTimeInfo() { return &_timeInfo; }
   
-
-
   void updateSensorData()
   {
     // バッテリ残量を取得
     _batteryLevel = M5.Power.getBatteryLevel();
     
-    // 温度と圧力を取得する
+    // 温度と圧力と高度と沸点を取得する
     _temperature = bmp.readTemperature();
     _pressure = bmp.readPressure();
+    _altitude = bmp.readAltitude();
+    _waterBoilingPoint = bmp.waterBoilingPoint((float) _pressure);
     
     // 現在時刻を取得
     if(!getLocalTime(&_timeInfo))
--- gokigen_geo_location/ShowGSIMap.hpp
+++ gokigen_geo_location/ShowGSIMap.hpp
@@ -35,6 +35,15 @@ public:
         return;
       }
 
+      // ----- 操作領域にマーキングを施す
+      M5.Display.setTextSize(1);
+      M5.Display.setFont(&fonts::efontJA_14_b);
+      M5.Display.setTextColor(TFT_WHITE, TFT_BLACK);
+      M5.Display.setCursor(308,0);
+      M5.Display.printf("Z");
+      M5.Display.drawRect(304,0, 15, 15, TFT_WHITE);
+
+      // ----- 緯度経度を取得
       double lat = gps.location.lat();
       double lng = gps.location.lng();
 
@@ -107,7 +116,7 @@ public:
         if (isHandled)
         {
           // ----- バイブレーション
-          makeVibration(VIBRATION_WEAK, VIBRATION_TIME_MIDDLE);
+          makeVibration(VIBRATION_MIDDLE, VIBRATION_TIME_SHORT);
           
           // ---- 開放する
           touchPos->resetPosition();
